e27228e0c02b2f552d22726179940d2bb7f8bfd5
Dima <zahrebelny@gmail.com>
2021-08-01 15:53:26 +0300

    added auth

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,42 @@
+import { get, set } from 'lodash';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SmartphoneModule } from './smartphone/smartphone.module';
-import { CompanyService } from './company/company.service';
-import { CompanyModule } from './company/company.module';
+import { AuthorModule } from './author/author.module';
+import { BookModule } from './book/book.module';
+import { UserModule } from './user/user.module';
+import { decode } from './utils/jwt.utils';
 
 @Module({
-  imports: [SmartphoneModule, CompanyModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nestjs-graphql'),
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      context: ({ req, res }) => {
+        // Get the cookie from request
+        const token = get(req, 'cookies.token');
+        console.log(req.headers);
+        console.log({ token });
+        // Verify the cookie
+
+        const user = token ? decode(get(req, 'cookies.token')) : null;
+        console.log(user);
+        // Attach the user object to the request object
+        if (user) {
+          set(req, 'user', user);
+        }
+
+        return { req, res };
+      },
+    }),
+
+    AuthorModule,
+    BookModule,
+    UserModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, CompanyService],
+  providers: [AppService],
 })
 export class AppModule {}
--- server/src/smartphone/smartphone.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SmartphoneService } from './smartphone.service';
-
-@Module({
-  providers: [SmartphoneService]
-})
-export class SmartphoneModule {}
